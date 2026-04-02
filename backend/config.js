82da2ed4ba8aef50d0daa074118be6d9211890fd
Smit Joshi <smit.joshi709@gmail.com>
2026-04-02 14:23:32 +0530

    --

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+const JWT_SECRET = "vlogwithsmit";
+module.exports = JWT_SECRET
+
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,27 @@
 const express = require("express");
-
+const zod = require("zod");
 const router = express.Router();
 
+//signup and sign in routes
 
+const signupSchema = zod.object({
+    username:zod.string(),
+    password: zod.string(),
+    firstName: zod.string(),
+    password: zod.string()
+})
+
+
+
+router.post("/signup",(req,res)=>{
+    const body = req.body;
+    const {success} = signupSchema.safeParse(req.body);
+    if(!success){
+        return res.json({
+            message: "Email already taken / incorrect inputs"
+        })
+    }
+    const user = User
+})
 
 module.exports = router;
