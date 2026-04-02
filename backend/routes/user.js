c1089f8a75943a456017470896fa73842bd8ab66
Smit Joshi <smit.joshi709@gmail.com>
2026-04-02 15:19:56 +0530

    working on middleware

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const zod = require("zod");
+const {User} = require("../db");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = require("../config");
 const router = express.Router();
 
 //signup and sign in routes
@@ -13,15 +16,32 @@ const signupSchema = zod.object({
 
 
 
-router.post("/signup",(req,res)=>{
-    const body = req.body;
+router.post("/signup", async (req,res)=>{
     const {success} = signupSchema.safeParse(req.body);
     if(!success){
-        return res.json({
+        return res.status(411).json({
             message: "Email already taken / incorrect inputs"
         })
     }
-    const user = User
+    const user = await User.findOne({
+        username: req.body.username
+    })
+
+    if(user._id){
+        return res.json({
+            message: "Email already taken / Incorrect inputs"
+        })
+    }
+
+    const dbUser =  await User.create(body);
+    const token = jwt.sign({
+        userId: dbUser._id
+    }, JWT_SECRET);
+
+    res.json({
+        message: "User created successfully",
+        token: token
+    })
 })
 
 module.exports = router;
